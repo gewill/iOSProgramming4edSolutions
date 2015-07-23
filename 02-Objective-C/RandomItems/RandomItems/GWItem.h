e564b80d5b86d27f088ef56ca002546c59776684
gewill <531sunlight@gmail.com>
2015-07-24 07:35:06 +0800

    add RandomItems

---
+++ 02-Objective-C/RandomItems/RandomItems/GWItem.h
@@ -0,0 +1,42 @@
+//
+//  GWItem.h
+//  RandomItems
+//
+//  Created by Will Ge on 7/23/15.
+//  Copyright © 2015 gewill.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GWItem : NSObject
+
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
++ (instancetype)randomItem;
+
+// GWItem 类的指定初始化方法
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
---
+++ Readme.md
@@ -0,0 +1,9 @@
+《iOS 编程》第四版 Demo 实现和习题，虽然都是书上直接的有的，但意义在于我自己手动实现了一遍。
+
+**本书的相关资料：**
+
+About the Book and Download Solutions:
+https://www.bignerdranch.com/we-write/ios-programming/
+
+中文注释项目代码见译者的 GitHub: https://github.com/dingdaojun/iOSProgramming4ed
+
